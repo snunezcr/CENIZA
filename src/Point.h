497719be1e103c4628f81e368eec0954eb77737d
Santiago Nunez-Corrales <snunezcr@gmail.com>
2010-12-20 19:39:45 -0600

    Initial setup and first class

---
+++ src/Point.h
@@ -0,0 +1,36 @@
+/*
+ * CENIZA: A multi-model ash dispersion simulation package
+ *
+ * Copyright (C) Red de Estaciones Meteorologicas 2010
+ * Author: Santiago Nunez Corrales <snunezcr@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+class Point {
+public:
+	Point();
+	Point(double xx, double yy, double zz);
+	Point(Point);
+	~Point();
+	Point& operator=(const Point &p);
+	Point operator+(const Point &p);
+	Point operator-(const Point &p);
+	Point operator*(double w);
+private:
+	double x;
+	double y;
+	double z;
+};
