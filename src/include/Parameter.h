a16d153d6f1193194764db1b824b41bf57cc74c9
Santiago Nunez-Corrales <snunezcr@gmail.com>
2011-01-26 22:33:53 -0600

    Add Parameter class

---
+++ src/include/Parameter.h
@@ -0,0 +1,68 @@
+/*
+ * CENIZA: A multi-model ash dispersion simulation package
+ *
+ * $BSD License start$
+ *
+ * Copyright (c) 2011, Red de Ciudadana de Estaciones Meteorologicas Costa Rica
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the
+ *       distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $BSD License end$
+ *
+ * Author: Santiago Nunez Corrales <snunezcr@gmail.com>
+ *
+ */
+
+#ifndef PARAMETER_H_
+#define PARAMETER_H_
+
+#include <Constants.h>
+#include <string>
+
+enum par_type {
+	PARAM_INT,
+	PARAM_DOUBLE
+};
+
+union par_data {
+	int data_int;
+	double data_double;
+};
+
+class Parameter {
+public:
+	Parameter(string name, int data);
+	Parameter(string name, double data);
+	~Parameter();
+	enum par_type getType() const;
+	union par_data getData() const;
+	const string getName() const;
+private:
+	string _name;
+	enum par_type _type;
+	union par_data _data;
+};
+
+#endif /* PARAMETER_H_ */
+
