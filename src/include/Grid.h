d563308fa3cf5cbe84ba1010af7aacf01cb6afb1
Santiago Nunez-Corrales <snunezcr@gmail.com>
2011-01-29 15:35:33 -0600

    Definition of Grid superclass. This class is the base class for any working implementation of a grid in any of its forms

---
+++ src/include/Grid.h
@@ -0,0 +1,59 @@
+/*
+ * CENIZA: A multi-model ash dispersion simulation package
+ *
+ * $BSD License start$
+ *
+ * Copyright (c) 2011, Red de Ciudadana de Estaciones Meteorologicas Costa Rica
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the
+ *       distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $BSD License end$
+ *
+ * Author: Santiago Nunez Corrales <snunezcr@gmail.com>
+ *
+ */
+
+#ifndef GRID_H_
+#define GRID_H_
+
+#include <Location.h>
+#include <vector>
+
+template <typename D> class Grid {
+public:
+	Grid();
+	~Grid();
+	virtual bool empty();
+	virtual bool member(Location<D> location);
+	virtual int size();
+	virtual void add(Location<D> location);
+	virtual void addBulk(vector< Location<D> > locations);
+	virtual Location<D> & next();
+	virtual void refine();
+	bool changed();
+protected:
+	bool _changed;
+};
+
+#endif /* GRID_H_ */
