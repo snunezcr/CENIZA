0bb44bc35199a4c48cc54d26640e9a6de36e9122
Santiago Nunez-Corrales <snunezcr@gmail.com>
2011-01-29 22:12:27 -0600

    Add SimpleGrid class

---
+++ src/include/SimpleGrid.h
@@ -0,0 +1,61 @@
+/*
+ * CENIZA: A multi-model ash dispersion simulation package
+ *
+ * $BSD License start$
+ *
+ * Copyright (c) 2011, Red de Ciudadana de Estaciones Meteorologicas Costa Rica
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the
+ *       distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $BSD License end$
+ *
+ * Author: Santiago Nunez Corrales <snunezcr@gmail.com>
+ *
+ */
+
+#ifndef SIMPLEGRID_H_
+#define SIMPLEGRID_H_
+
+#include <Grid.h>
+
+template <typename D> class SimpleGrid: public Grid {
+public:
+	SimpleGrid(Boundary bounds, int m, int n);
+	~SimpleGrid();
+	bool empty();
+	bool member(Location<D> location);
+	int size();
+	void add(Location<D> location);
+	void add(vector< Location<D> > locations);
+	Location<D> & next();
+	void refine();
+	bool changed();
+private:
+	double _delta;
+	int _m;
+	int _n;
+	vector< Location<D> > _locations;
+};
+
+#endif /* SIMPLEGRID_H_ */
